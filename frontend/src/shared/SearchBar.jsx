a2304453504553832a4bdb10e88d0d3b32cd4b72
arjunonair <arjunonair123@gmail.com>
2023-04-06 13:28:45 +0530

    searchbar completed

--- frontend/src/shared/SearchBar.jsx
+++ frontend/src/shared/SearchBar.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import './search-bar.css'
 import {Col,Form,FormGroup} from "reactstrap";
 
 const SearchBar = () => {
-  return <Col lg='12'>
+
+    const locationRef = useRef('');
+    const distanceRef = useRef(0);
+    const maxGroupSizeRef = useRef(0);
+
+    const searchHandler =()=> {
+        const maxGroupSize = maxGroupSizeRef.current.value;
+        const location = locationRef.current.value;
+        const distance = distanceRef.current.value;
+
+        if(location ==='' || distance === '' || maxGroupSize === '')
+        {
+            return alert('All fields are mandatory..!');
+        }
+};
+
+  return (
+    <Col lg='12'>
     <div className='search__bar'>
-        <Form className='d-flex align-items-center gap-4'>
+        <Form className='d-flex align-items-center gap-4 form'>
             <FormGroup className='d-flex gap-3 form__group form__group-fast'>
                 <span>
                 <i class="ri-map-pin-line"></i>
                 </span>
                 <div>
                     <h6>Location</h6>
-                    <input type="text" placeholder='where are you going?' />
+                    <input type="text" placeholder='where are you going?' ref={locationRef}/>
                 </div>
             </FormGroup>
             <FormGroup className='d-flex gap-3 form__group form__group-fast'>
@@ -21,7 +38,7 @@ const SearchBar = () => {
                 </span>
                 <div>
                     <h6>Distance</h6>
-                    <input type="number" placeholder='Distance K/m' />
+                    <input type="number" placeholder='Distance K/m' ref={distanceRef}/>
                 </div>
             </FormGroup>
             <FormGroup className='d-flex gap-3 form__group form__group-last'>
@@ -30,12 +47,17 @@ const SearchBar = () => {
                 </span>
                 <div>
                     <h6>Max people</h6>
-                    <input type="number" placeholder='0' />
+                    <input type="number" placeholder='0' ref={maxGroupSizeRef}/>
                 </div>
+                
             </FormGroup>
+            <span className='search__icon' type= 'submit' onClick={searchHandler}>
+                <i class="ri-search-line"></i>
+            </span>
         </Form>
     </div>
     </Col>
+  )
 }
 
 export default SearchBar
